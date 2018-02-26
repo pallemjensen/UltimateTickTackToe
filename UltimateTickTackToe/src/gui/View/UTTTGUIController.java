b3c2d1ac9546277d794b797c615bd4617fb34607
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-02-26 10:44:36 +0100

    including Mads' stuff

---
+++ UltimateTickTackToe/src/gui/View/UTTTGUIController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui.View;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.GridPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author pmj
+ */
+public class UTTTGUIController implements Initializable {
+
+    @FXML
+    private Label showNextPlayer;
+    @FXML
+    private MenuItem choice1;
+    @FXML
+    private GridPane macroBoard;
+    @FXML
+    private GridPane microOne;
+    @FXML
+    private GridPane microTwo;
+    @FXML
+    private GridPane MicroThree;
+    @FXML
+    private GridPane MicroFour;
+    @FXML
+    private GridPane MicroFive;
+    @FXML
+    private GridPane microSix;
+    @FXML
+    private GridPane microSeven;
+    @FXML
+    private GridPane microEight;
+    @FXML
+    private GridPane microNine;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
